ba5033118a2ad9f618f318697216c52508a4fa97
Gloorf <glorf@glorf.fr>
2015-09-19 16:48:06 +0200

    Fixed csv_sender.py !

--- csv_sender.py
+++ csv_sender.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 # -*- coding: utf8 -*-
-from config import config as c
-import util
+from poewatcher import config as c
+from poewatcher import utils
 """
 Use only once, to send a whole CSV file. Ignores first line (assumed to be headers)
 """
 with open(c.get("map_recorder", "output_path"), "r", encoding='utf-8') as file:
     file.readline() #Go past first line[headers]
     for line in file:
-        k = util.contact_server(util.dict_from_csv(line))
+        k = utils.contact_server(utils.dict_from_csv(line))
         print("Received : {0}".format(k))
